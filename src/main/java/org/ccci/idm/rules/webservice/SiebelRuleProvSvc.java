47f535bf3463d9f2b1f6e70846324c5a112479eb
nathan.kopp@ccci.org <nathan.kopp@ccci.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2011-12-21 21:34:18 +0000

    IDM-644 Receive emplid only, then fetch data from HR system via web service client.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/identity/idm-services2/rule-based-provisioning-services@10137 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/idm/rules/webservice/SiebelRuleProvSvc.java
+++ src/main/java/org/ccci/idm/rules/webservice/SiebelRuleProvSvc.java
@@ -1,5 +1,7 @@
 package org.ccci.idm.rules.webservice;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 import java.util.Properties;
 
@@ -7,6 +9,7 @@ import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.namespace.QName;
 
 import org.ccci.idm.authentication.credentials.impl.UsernamePasswordCredentials;
 import org.ccci.idm.authentication.handler.impl.PropertyBasedUsernamePasswordAuthHandler;
@@ -15,7 +18,11 @@ import org.ccci.idm.authentication.manager.impl.AuthenticationManagerImpl;
 import org.ccci.idm.rules.processes.RuleBasedRoleProvisioningProcess;
 import org.ccci.idm.rules.services.RoleManagerServiceGrouper;
 import org.ccci.soa.obj.USEmployment;
+import org.ccci.soa.pshr.client.StaffService;
+import org.ccci.soa.pshr.client.StaffServiceService;
+import org.ccci.soa.pshr.client.UsStaffMember;
 import org.ccci.util.properties.PropertiesWithFallback;
+import org.ccci.util.properties.CcciProperties.PropertyEncryptionSetup;
 
 @WebService()
 @Stateless()
@@ -24,23 +31,19 @@ public class SiebelRuleProvSvc
     private AuthenticationManager authenticationManager;
     private Properties properties;
     
-	RuleBasedRoleProvisioningProcess ruleBasedResponsibilityProvisioningService;
-	RuleBasedRoleProvisioningProcess ruleBasedAccessGroupProvisioningService;
+    private RuleBasedRoleProvisioningProcess ruleBasedResponsibilityProvisioningService;
+	private RuleBasedRoleProvisioningProcess ruleBasedAccessGroupProvisioningService;
+	private StaffService service;
+    private String serviceServerId;
+    private String serviceServerSecret;
 
-	public SiebelRuleProvSvc()
+	public SiebelRuleProvSvc() throws Exception
 	{
 		super();
-		properties = new PropertiesWithFallback(false, "/ora/config/siebelRuleProvSvc.properties","/default.properties");
-        authenticationManager = new AuthenticationManagerImpl();
-        authenticationManager.addAuthenticationHandler(new PropertyBasedUsernamePasswordAuthHandler(properties, "user."));
-
-		ruleBasedResponsibilityProvisioningService = new RuleBasedRoleProvisioningProcess(new RoleManagerServiceGrouper("siebel.responsibility.rules@ccci.org", "ccci:itroles:uscore:siebel:resp", true));
-		ruleBasedResponsibilityProvisioningService.addExcelRuleset("classpath:SiebelResponsibilityProvisioningRules.xls", "Sheet1");
-		ruleBasedResponsibilityProvisioningService.addDrlRuleset("classpath:RemoveAllRoles.drl");
-
-		ruleBasedAccessGroupProvisioningService = new RuleBasedRoleProvisioningProcess(new RoleManagerServiceGrouper("siebel.accessgroup.rules@ccci.org", "ccci:itroles:uscore:siebel:access_groups", true));
-		ruleBasedAccessGroupProvisioningService.addExcelRuleset("classpath:SiebelAccessGroupProvisioningRules.xls", "Sheet1");
-		ruleBasedAccessGroupProvisioningService.addDrlRuleset("classpath:RemoveAllRoles.drl");
+		setupProperties();
+		setupAuthenticationManager();
+		setupRules();
+		setupStaffServiceClient();
 	}
 
 	public SiebelRuleProvSvc(RuleBasedRoleProvisioningProcess ruleBasedResponsibilityProvisioningService, RuleBasedRoleProvisioningProcess ruleBasedAccessGroupProvisioningService)
@@ -52,22 +55,61 @@ public class SiebelRuleProvSvc
 	}
 
 	@WebMethod(operationName = "provisionSiebelResponsibilityAccessForEmployee")
-	public String provisionSiebelResponsibilityAccessForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "globalId") String globalId, @WebParam(name = "employment") USEmployment employment) throws Exception
+	public String provisionSiebelResponsibilityAccessForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "ssoGuid") String ssoGuid, @WebParam(name = "emplid") String emplid) throws Exception
 	{
 	    authenticationManager.authenticate(new UsernamePasswordCredentials(serverId, serverSecret));
-	    
-		ruleBasedResponsibilityProvisioningService.computeAndApplyRolesForUser(globalId, new Date(), employment);
-
+	    UsStaffMember staff = service.getStaff(serviceServerId, serviceServerSecret, emplid);
+	    if(staff==null) throw new RuntimeException("Emplid is not valid.");
+		ruleBasedResponsibilityProvisioningService.computeAndApplyRolesForUser(ssoGuid, new Date(), staff.getEmploymentInfo());
 		return null;
 	}
 
 	@WebMethod(operationName = "provisionSiebelAccessGroupAccessForEmployee")
-	public String provisionSiebelAccessGroupAccessForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "globalId") String globalId, @WebParam(name = "employment") USEmployment employment) throws Exception
+	public String provisionSiebelAccessGroupAccessForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "ssoGuid") String ssoGuid, @WebParam(name = "emplid") String emplid) throws Exception
 	{
 	    authenticationManager.authenticate(new UsernamePasswordCredentials(serverId, serverSecret));
-	    
-		ruleBasedAccessGroupProvisioningService.computeAndApplyRolesForUser(globalId, new Date(), employment);
-
+	    UsStaffMember staff = service.getStaff(serviceServerId, serviceServerSecret, emplid);
+	    if(staff==null) throw new RuntimeException("Emplid is not valid.");
+		ruleBasedAccessGroupProvisioningService.computeAndApplyRolesForUser(ssoGuid, new Date(), staff.getEmploymentInfo());
 		return null;
 	}
+	
+	
+	private void setupProperties()
+    {
+        PropertyEncryptionSetup encryptionSetup = new PropertyEncryptionSetup("lco97gf5t7D%Y4bh89%U34IF&l*()$Hg6wRD^j4");
+        properties = new PropertiesWithFallback(encryptionSetup, false, "/ora/config/siebelRuleProvSvc.properties","/default.properties");
+    }
+
+    private void setupRules()
+    {
+        ruleBasedResponsibilityProvisioningService = new RuleBasedRoleProvisioningProcess(new RoleManagerServiceGrouper("siebel.responsibility.rules@ccci.org", "ccci:itroles:uscore:siebel:resp", true));
+        ruleBasedResponsibilityProvisioningService.addExcelRuleset("classpath:SiebelResponsibilityProvisioningRules.xls", "Sheet1");
+        ruleBasedResponsibilityProvisioningService.addDrlRuleset("classpath:RemoveAllRoles.drl");
+
+        ruleBasedAccessGroupProvisioningService = new RuleBasedRoleProvisioningProcess(new RoleManagerServiceGrouper("siebel.accessgroup.rules@ccci.org", "ccci:itroles:uscore:siebel:access_groups", true));
+        ruleBasedAccessGroupProvisioningService.addExcelRuleset("classpath:SiebelAccessGroupProvisioningRules.xls", "Sheet1");
+        ruleBasedAccessGroupProvisioningService.addDrlRuleset("classpath:RemoveAllRoles.drl");
+    }
+
+    private void setupAuthenticationManager()
+    {
+        authenticationManager = new AuthenticationManagerImpl();
+        authenticationManager.addAuthenticationHandler(new PropertyBasedUsernamePasswordAuthHandler(properties, "user."));
+    }
+
+    private void setupStaffServiceClient() throws MalformedURLException
+    {
+        String wsdlUrl = properties.getProperty("staffService.wsdlUrl");
+        String namespace = properties.getProperty("staffService.namespace");
+        String serviceName = properties.getProperty("staffService.serviceName");
+        URL wsdl = new URL(wsdlUrl);
+        QName serviceQname = new QName(namespace, serviceName);
+        
+        StaffServiceService locator = new StaffServiceService(wsdl, serviceQname);
+        service = locator.getStaffServicePort();
+        
+        serviceServerId = properties.getProperty("staffService.serverId");
+        serviceServerSecret = properties.getProperty("staffService.serverSecret");
+    }
 }
--- src/main/java/org/ccci/idm/rules/webservice/StaffWebRuleProvSvc.java
+++ src/main/java/org/ccci/idm/rules/webservice/StaffWebRuleProvSvc.java
@@ -1,5 +1,7 @@
 package org.ccci.idm.rules.webservice;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 import java.util.Properties;
 
@@ -7,6 +9,7 @@ import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.namespace.QName;
 
 import org.ccci.idm.authentication.credentials.impl.UsernamePasswordCredentials;
 import org.ccci.idm.authentication.handler.impl.PropertyBasedUsernamePasswordAuthHandler;
@@ -14,7 +17,10 @@ import org.ccci.idm.authentication.manager.AuthenticationManager;
 import org.ccci.idm.authentication.manager.impl.AuthenticationManagerImpl;
 import org.ccci.idm.rules.processes.RuleBasedRoleProvisioningProcess;
 import org.ccci.idm.rules.services.RoleManagerServiceGrouper;
-import org.ccci.soa.obj.USEmployment;
+import org.ccci.soa.pshr.client.StaffService;
+import org.ccci.soa.pshr.client.StaffServiceService;
+import org.ccci.soa.pshr.client.UsStaffMember;
+import org.ccci.util.properties.CcciProperties.PropertyEncryptionSetup;
 import org.ccci.util.properties.PropertiesWithFallback;
 
 @WebService()
@@ -24,33 +30,74 @@ public class StaffWebRuleProvSvc
     private AuthenticationManager authenticationManager;
     private Properties properties;
     
-	RuleBasedRoleProvisioningProcess ruleBasedStaffWebProvisioningService;
+	private RuleBasedRoleProvisioningProcess ruleBasedStaffWebProvisioningService;
+	private StaffService service;
+	private String serviceServerId;
+	private String serviceServerSecret;
+	
 
-	public StaffWebRuleProvSvc()
+	public StaffWebRuleProvSvc() throws Exception
 	{
 		super();
-		properties = new PropertiesWithFallback(false, "/ora/config/staffWebRuleProvSvc.properties","/default.properties");
-        authenticationManager = new AuthenticationManagerImpl();
-        authenticationManager.addAuthenticationHandler(new PropertyBasedUsernamePasswordAuthHandler(properties, "user."));
+		setupProperties();
+        setupAuthenticationManager();
+        setupRules();
+        setupStaffServiceClient();
+	}
 
+	public StaffWebRuleProvSvc(RuleBasedRoleProvisioningProcess ruleBasedStaffWebProvisioningService, StaffService service)
+	{
+		super();
+		this.ruleBasedStaffWebProvisioningService = ruleBasedStaffWebProvisioningService;
+		this.service = service;
+	}
+
+    @WebMethod(operationName = "provisionSiebelResponsibilityAccessForEmployee")
+    public String provisionStaffWebConsumerForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "ssoGuid") String ssoGuid, @WebParam(name = "emplid") String emplid) throws Exception
+    {
+        authenticationManager.authenticate(new UsernamePasswordCredentials(serverId, serverSecret));
+        UsStaffMember staff = service.getStaff(serviceServerId, serviceServerSecret, emplid);
+        if(staff==null) throw new RuntimeException("Emplid is not valid.");
+        ruleBasedStaffWebProvisioningService.computeAndApplyRolesForUser(ssoGuid, new Date(), staff.getEmploymentInfo());
+        return null;
+    }
+    
+    
+    
+    
+    
+    private void setupProperties()
+    {
+        PropertyEncryptionSetup encryptionSetup = new PropertyEncryptionSetup("lco97gf5t7D%Y4bh89%U34IF&l*()$Hg6wRD^j4");
+        properties = new PropertiesWithFallback(encryptionSetup, false, "/ora/config/staffWebRuleProvSvc.properties","/default.properties");
+    }
+
+    private void setupRules()
+    {
         ruleBasedStaffWebProvisioningService = new RuleBasedRoleProvisioningProcess(new RoleManagerServiceGrouper("staffweb.responsibility.rules@ccci.org", "", false));
         ruleBasedStaffWebProvisioningService.addDrlRuleset("classpath:StaffWebAccess.drl");
-	}
+    }
 
-	public StaffWebRuleProvSvc(RuleBasedRoleProvisioningProcess ruleBasedStaffWebProvisioningService)
-	{
-		super();
+    private void setupAuthenticationManager()
+    {
+        authenticationManager = new AuthenticationManagerImpl();
+        authenticationManager.addAuthenticationHandler(new PropertyBasedUsernamePasswordAuthHandler(properties, "user."));
+    }
 
-		this.ruleBasedStaffWebProvisioningService = ruleBasedStaffWebProvisioningService;
-	}
+    private void setupStaffServiceClient() throws MalformedURLException
+    {
+        String wsdlUrl = properties.getProperty("staffService.wsdlUrl");
+        String namespace = properties.getProperty("staffService.namespace");
+        String serviceName = properties.getProperty("staffService.serviceName");
+        URL wsdl = new URL(wsdlUrl);
+        QName serviceQname = new QName(namespace, serviceName);
+        
+        StaffServiceService locator = new StaffServiceService(wsdl, serviceQname);
+        service = locator.getStaffServicePort();
+        
+        serviceServerId = properties.getProperty("staffService.serverId");
+        serviceServerSecret = properties.getProperty("staffService.serverSecret");
+    }
 
-	@WebMethod(operationName = "provisionSiebelResponsibilityAccessForEmployee")
-	public String provisionStaffWebConsumerForEmployee(@WebParam(name = "serverId") String serverId, @WebParam(name = "serverSecret") String serverSecret, @WebParam(name = "globalId") String globalId, @WebParam(name = "employment") USEmployment employment) throws Exception
-	{
-	    authenticationManager.authenticate(new UsernamePasswordCredentials(serverId, serverSecret));
-	    
-	    ruleBasedStaffWebProvisioningService.computeAndApplyRolesForUser(globalId, new Date(), employment);
 
-		return null;
-	}
 }
