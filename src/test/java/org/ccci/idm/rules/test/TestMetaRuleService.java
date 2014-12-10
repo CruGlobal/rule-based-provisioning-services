4567c9a4f5914637cea86bde7a1d8ee7bb3f7d40
Lee Braddock <lee.braddock@cru.org>
2014-12-10 16:44:13 -0500

    Change property name from grouperBase to base.

--- src/test/java/org/ccci/idm/rules/test/TestMetaRuleService.java
+++ src/test/java/org/ccci/idm/rules/test/TestMetaRuleService.java
@@ -19,19 +19,19 @@ public class TestMetaRuleService
         Properties props = new Properties();
         props.setProperty("rulesets", "stellent, siebel-resp, siebel-access");
         props.setProperty("stellent.attestationUser", "stellent.rules@ccci.org");
-        props.setProperty("stellent.grouperBase", "ccci:itroles:uscore:stellent");
+        props.setProperty("stellent.base", "ccci:itroles:uscore:stellent");
         props.setProperty("stellent.rulefiles", "classpath:StaffWebAccess.drl, classpath:StellentRules.xls");
         props.setProperty("stellent.classpath_StellentRules.xls.sheets", "Sheet1");
         props.setProperty("stellent.facts", "EmployeeInfo, IdentityUser");
         
         props.setProperty("siebel-resp.attestationUser", "siebel.rules@ccci.org");
-        props.setProperty("siebel-resp.grouperBase", "ccci:itroles:uscore:siebel:resp");
+        props.setProperty("siebel-resp.base", "ccci:itroles:uscore:siebel:resp");
         props.setProperty("siebel-resp.rulefiles", "classpath:SiebelResponsibilityProvisioningRules.xls, classpath:RemoveAllRoles.drl");
         props.setProperty("siebel-resp.classpath_SiebelResponsibilityProvisioningRules.xls.sheets", "Sheet1");
         props.setProperty("siebel-resp.facts", "EmployeeInfo, IdentityUser");
         
         props.setProperty("siebel-access.attestationUser", "siebel.rules@ccci.org");
-        props.setProperty("siebel-access.grouperBase", "ccci:itroles:uscore:siebel:resp");
+        props.setProperty("siebel-access.base", "ccci:itroles:uscore:siebel:resp");
         props.setProperty("siebel-access.rulefiles", "classpath:SiebelAccessGroupProvisioningRules.xls, classpath:RemoveAllRoles.drl");
         props.setProperty("siebel-access.classpath_SiebelAccessGroupProvisioningRules.xls.sheets", "Sheet1");
         props.setProperty("siebel-access.facts", "EmployeeInfo, IdentityUser");
